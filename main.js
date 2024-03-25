e74a832a12261646cc38c31f577bc990002849cc
amirshayanyian <wahtamir12@gmail.com>
2024-03-25 18:28:16 +0330

    created main function to start the server

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+async function main() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
+
+main();
