9904def8eadc86821ab497370120eebadca02d84
amirshayanyian <wahtamir12@gmail.com>
2024-03-26 15:38:24 +0330

    created empty controller methods for OTP

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,20 @@
+class AuthController {
+  #service;
+  // constructor(
+
+  // )
+
+  static async sendOTP(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async checkOTP(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
