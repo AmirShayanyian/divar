95cd7164b0f1b0c4a9f158131fe5902f3585ab2b
amirshayanyian <wahtamir12@gmail.com>
2024-03-26 13:45:44 +0330

    made project structure

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
+
 async function main() {
   const app = express();
   const port = process.env.PORT || 3000;
+
   app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
   });
