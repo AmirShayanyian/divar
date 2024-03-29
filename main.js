0a33a2ddcec6cde147c59d1ac265c728cd3750f7
amirshayanyian <wahtamir12@gmail.com>
2024-03-29 11:08:54 +0330

    added json and urlencoded req formats (:

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 const mongooseConnection = require('./src/config/mongoose.config');
 const swaggerConfig = require('./src/config/swagger.config');
-const  NotFoundHandler  = require('./src/common/exception/not-found.handler');
+const NotFoundHandler = require('./src/common/exception/not-found.handler');
 const GlobalExceptionHandler = require('./src/common/exception/global-exception.handler');
 async function main() {
   const app = express();
   const port = process.env.PORT || 3000;
   await mongooseConnection();
   swaggerConfig(app);
-
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use(mainRouter);
   NotFoundHandler(app);
-  GlobalExceptionHandler(app)
+  GlobalExceptionHandler(app);
   app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
   });
